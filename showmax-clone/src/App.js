612ec658a0227fc8264e8419aaf4ac8b73ba9e45
sange371 <zusangembaleki@icloud.com>
2025-12-22 18:15:15 +0200

    added a new file

--- showmax-clone/src/App.js
+++ showmax-clone/src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
+import Movies from './Movies';
 
 function App() {
   return (
     <div className="App">
+     
       <h1>Showmax Clone</h1>
       <form>
         <input type="search" placeholder="search for movies or series" className="search" />
         <input type="submit" value="Search" className="search-button"/>
       </form>
+      <Movies />
     </div>
   );
 }
