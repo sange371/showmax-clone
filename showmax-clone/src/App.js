920aca9b7c48c98a9f62a354421c1fa9c84924ce
sange371 <zusangembaleki@icloud.com>
2025-12-22 17:37:14 +0200

    added basic code

---
+++ showmax-clone/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Showmax Clone</h1>
+      <form>
+        <input type="search" placeholder="search for movies or series" className="search" />
+        <input type="submit" value="Search" className="search-button"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
